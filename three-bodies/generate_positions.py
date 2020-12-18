c99b034b82d1429ccb99a9b3b139d52dd1f0d022
alessandrobessi <alessandro.bessi@mail.com>
2020-12-18 16:56:29 +0100

    first working, unstable configuration

---
+++ three-bodies/generate_positions.py
@@ -0,0 +1,105 @@
+import json
+import math
+import scipy
+import numpy as np
+import scipy.integrate
+from pathlib import Path
+
+#  universal gravitation constant
+G = 6.67408e-11  # N-m2/kg2
+
+# Reference quantities
+m_nd = 1.989e+30  # kg #mass of the sun
+r_nd = 5.326e+12  # m #distance between stars in Alpha Centauri
+v_nd = 30000  # m/s #relative velocity of earth around the sun
+t_nd = 79.91*365*24*3600*0.51  # s #orbital period of Alpha Centauri
+
+# Net constants
+K1 = G*t_nd*m_nd/(r_nd**2*v_nd)
+K2 = v_nd*t_nd/r_nd
+
+# Define masses
+m1 = 1
+m2 = 1
+m3 = 1
+m4 = 0.0000001
+
+# Initial positions
+r1 = np.array([-2, 0, 0], dtype="float64")
+r2 = np.array([2, 0, 0], dtype="float64")
+r3 = np.array([0, math.sqrt(3), 0], dtype="float64")
+r4 = np.array([0, 1, 0], dtype="float64")
+
+# Initial velocities
+v1 = np.array([0.01, 0.01, 0], dtype="float64")
+v2 = np.array([-0.01, 0, 0], dtype="float64")
+v3 = np.array([0.01, -0.01, 0], dtype="float64")
+v4 = np.array([0, 0, 0], dtype="float64")
+
+
+def four_body_equations(w, t, G, m1, m2, m3, m4):
+    r2 = w[3:6]
+    r1 = w[:3]
+    r3 = w[6:9]
+    r4 = w[9:12]
+    v1 = w[12:15]
+    v2 = w[15:18]
+    v3 = w[18:21]
+    v4 = w[21:24]
+    
+    r12 = np.linalg.norm(r2-r1)
+    r13 = np.linalg.norm(r3-r1)
+    r14 = np.linalg.norm(r4-r1)
+    r23 = np.linalg.norm(r3-r2)
+    r24 = np.linalg.norm(r4-r2)
+    r34 = np.linalg.norm(r4-r3)
+    
+    dv1bydt = K1*m2*(r2-r1)/r12**3+K1*m3*(r3-r1)/r13**3+K1*m4*(r4-r1)/r14**3
+    dv2bydt = K1*m1*(r1-r2)/r12**3+K1*m3*(r3-r2)/r23**3+K1*m4*(r4-r2)/r24**3
+    dv3bydt = K1*m1*(r1-r3)/r13**3+K1*m2*(r2-r3)/r23**3+K1*m4*(r4-r3)/r34**3
+    dv4bydt = K1*m1*(r1-r4)/r14**3+K1*m2*(r2-r4)/r24**3+K1*m3*(r3-r4)/r34**3
+
+    dr1bydt = K2*v1
+    dr2bydt = K2*v2
+    dr3bydt = K2*v3
+    dr4bydt = K2*v4
+    
+    r_derivs = np.concatenate((dr1bydt, dr2bydt, dr3bydt, dr4bydt))
+    v_derivs = np.concatenate((dv1bydt, dv2bydt, dv3bydt, dv4bydt))
+    derivs = np.concatenate((r_derivs, v_derivs))
+    return derivs
+
+
+init_params = np.array([r1, r2, r3, r4, v1, v2, v3, v4]).flatten()
+
+time_span = np.linspace(0, 1600, 32000)
+solutions = scipy.integrate.odeint(four_body_equations, init_params, time_span, args=(G, m1, m2, m3, m4))
+
+d = {}
+for i in range(solutions.shape[0]):
+    d[i] = {
+        "p1": {
+            "x": solutions[i, 0],
+            "y": solutions[i, 1],
+            "z": solutions[i, 2],
+        },
+        "p2": {
+            "x": solutions[i, 3],
+            "y": solutions[i, 4],
+            "z": solutions[i, 5],
+        },
+        "p3": {
+            "x": solutions[i, 6],
+            "y": solutions[i, 7],
+            "z": solutions[i, 8],
+        },
+        "p4": {
+            "x": solutions[i, 9],
+            "y": solutions[i, 10],
+            "z": solutions[i, 11],
+        },
+    }
+
+path_to_file = Path() / 'positions.json'
+with open(path_to_file, 'w') as fp:
+    json.dump(d, fp)
---
+++ three-bodies/three-bodies.js
@@ -0,0 +1,111 @@
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+
+let url = 'positions.json';
+let positions;
+let time = 0;
+let id;
+
+fetch(url)
+	.then(res => res.json())
+	.then((out) => {
+		positions = out;
+		try {
+			animate();
+		} catch {
+			cancelAnimationFrame(id);
+		}
+	})
+	.catch(err => {
+		throw err;
+	});
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer({ "antialias": true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.SphereGeometry(0.1, 32, 32);
+var geometrySmall = new THREE.SphereGeometry(0.05, 32, 32);
+var material = new THREE.MeshNormalMaterial();
+
+var planet1 = new THREE.Mesh(geometry, material);
+planet1.position.x -= 0.5
+scene.add(planet1);
+
+var planet2 = new THREE.Mesh(geometry, material);
+planet2.position.x += 0.5
+scene.add(planet2);
+
+var planet3 = new THREE.Mesh(geometry, material);
+planet3.position.y += 1
+scene.add(planet3);
+
+var planet4 = new THREE.Mesh(geometrySmall, material);
+planet4.position.y += 0.5
+planet4.position.z += 0.5
+scene.add(planet4);
+
+camera.position.z = 20;
+camera.position.y += 5;
+
+window.addEventListener('keydown', onKeyDown, false);
+
+var animate = function () {
+	id = requestAnimationFrame(animate);
+
+	let i = parseInt(time);
+	planet1.position.x = positions[i]["p1"].x
+	planet1.position.y = positions[i]["p1"].y
+	planet1.position.z = positions[i]["p1"].z
+
+	planet2.position.x = positions[i]["p2"].x
+	planet2.position.y = positions[i]["p2"].y
+	planet2.position.z = positions[i]["p2"].z
+
+	planet3.position.x = positions[i]["p3"].x
+	planet3.position.y = positions[i]["p3"].y
+	planet3.position.z = positions[i]["p3"].z
+
+	planet4.position.x = positions[i]["p4"].x
+	planet4.position.y = positions[i]["p4"].y
+	planet4.position.z = positions[i]["p4"].z
+
+	const t = Date.now() * 0.001;
+	planet1.rotation.x = t * 0.1;
+	planet1.rotation.y = t * 0.1;
+	planet2.rotation.x = t * 0.1;
+	planet2.rotation.y = t * 0.1;
+	planet3.rotation.x = t * 0.1;
+	planet3.rotation.y = t * 0.1;
+	planet4.rotation.x = t * 0.1;
+	planet4.rotation.y = t * 0.1;
+
+	time++;
+
+	renderer.render(scene, camera);
+};
+
+function onKeyDown(e) {
+	switch (e["key"]) {
+		case "ArrowUp":
+			camera.position.y += 0.1;
+			break;
+		case "ArrowDown":
+			camera.position.y -= 0.1;
+			break;
+		case "ArrowRight":
+			camera.position.x += 0.1;
+			break;
+		case "ArrowLeft":
+			camera.position.x -= 0.1;
+			break;
+		case "a":
+			camera.position.z += 0.1;
+			break;
+		case "z":
+			camera.position.z -= 0.1;
+			break;
+	}
+}
